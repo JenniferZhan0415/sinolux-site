23f60eb40513f30e340d8ce49544b3435697620e
modalsoul0226 <modalsoul0226@gmail.com>
2021-01-09 14:24:31 -0500

    fix: update exhibition grid layout

--- src/components/exhibitions/exhibitions.js
+++ src/components/exhibitions/exhibitions.js
@@ -3,6 +3,7 @@ import React, {Component} from "react"
 import styles from "./exhibitions.module.scss"
 
 import Year2021 from "./2021/year"
+import Year2020 from "./2020/year"
 
 const introductions = {
     "jebel": <div>haha</div>,
@@ -36,17 +37,32 @@ class Exhibitions extends Component {
 
         return (
             <div className={styles.container + " text-normal"}>
-                {/* navigation */}
-                <div className={styles.navigation}>
-                    <Year2021 setIntroId={this.setIntroId}/>
-                    <Year2021/>
-                    <Year2021/>
-                    <Year2021/>
-                    <Year2021/>
+                {/* header */}
+                <div className={styles.header}>
+                    <div className={styles.headerNav}>
+                        <div className={styles.headerY}>
+                            <div style={{textAlign:"right"}} className={"bold red"}>Y</div>
+                            <div className={styles.headerM}>
+                                <div className={"bold yellow"}>M</div>
+                                <div className={"bold green"}>Programme / Films</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"bold blue"}>Info</div>
                 </div>
-    
                 {/* content */}
-                {content}
+                <div className={styles.content}>
+                    {/* navigation */}
+                    <div className={styles.navigation}>
+                        <Year2021 setIntroId={this.setIntroId}/>
+                        <Year2020/>
+                        <Year2021/>
+                        <Year2021/>
+                        <Year2021/>
+                    </div>
+                    {/* content */}
+                    {content}
+                </div>
             </div>
         )
     }
