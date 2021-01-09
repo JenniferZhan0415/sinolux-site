e800ec52df5fb40f0db16049a06dce47d283a66e
modalsoul0226 <modalsoul0226@gmail.com>
2021-01-09 14:25:34 -0500

    feat: add exhibition 2020

---
+++ src/components/exhibitions/2020/months/dec.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react"
+
+import styles from "../../exhibitions.module.scss"
+
+class Dec extends Component {
+    setId(id) {
+        this.props.setIntroId(id)
+    }
+
+    render() {
+        return (
+            <div className={styles.month}>
+                <div>
+                    <div className={styles.sticky}>December</div>
+                </div>
+                <div className={styles.titles}>
+                    <p>
+                        With<br/>
+                        <span className="italic">Pung-leung Kwan</span>
+                    </p>
+                    <button className={styles.title} onClick={this.setId.bind(this, 'wind')} onKeyDown={this.setId.bind(this, 'wind')}>
+                        <span className="bold">Let the Wind Carry Me</span><br/>
+                        Y: 2009<br/>
+                        D: Hsiu-chiung Chiang & Pung-leung Kwan
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Dec
---
+++ src/components/exhibitions/2020/months/nov.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react"
+
+import styles from "../../exhibitions.module.scss"
+
+class Nov extends Component {
+    setId(id) {
+        this.props.setIntroId(id)
+    }
+
+    render() {
+        return (
+            <div className={styles.month}>
+                <div>
+                    <div className={styles.sticky}>November</div>
+                </div>
+                <div className={styles.titles}>
+                    <p>
+                        Programme<br/>
+                        With <span className="italic">Norris Wong</span>
+                    </p>
+                    <button className={styles.title} onClick={this.setId.bind(this, 'edward')} onKeyDown={this.setId.bind(this, 'edward')}>
+                        <span className="bold">My Prince Edward</span><br/>
+                        Y: 2019<br/>
+                        D: Norris Wong
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Nov
