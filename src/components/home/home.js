0f530d18834b76d3799bc39b7e62b05ed407e8bb
modalsoul0226 <modalsoul0226@gmail.com>
2021-02-19 10:56:14 -0500

    fix: remove redundant style imports

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -12,7 +12,6 @@ import Footer from "../footer/footer"
 import Cover from "./cover/cover"
 
 import styles from "./home.module.scss"
-import "./home.css"
 
 class Home extends Component {
     constructor(props) {
