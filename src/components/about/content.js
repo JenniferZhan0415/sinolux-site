7120098e207c87098bc5876e991d79a391910500
modalsoul0226 <modalsoul0226@gmail.com>
2021-04-22 12:39:26 -0400

    fix: make the about page more responsive

--- src/components/about/content.js
+++ src/components/about/content.js
@@ -21,7 +21,6 @@ const about = [
         我们的名字 “华光” 来源于两个拉丁词汇 —— 'SINO' 和 'LUX'，分别代表着 “华语” 和 “光”，意即用优秀的华语电影点亮中华文化之光。
     </p>,
     <p>
-        <br/>
         <span className="italic">SINOLUX</span> is a made-up word consisting of two Latin noun phrases, <span className="italic">SINO</span> and <span className="italic">LUX</span> which means <span className="italic">Chinese</span> and <span className="italic">Light</span> respectively. Together, they demonstrate our motivation and determination of bringing the finest Chinese-language films to Canadian audience.
     </p>
 ]
@@ -29,89 +28,98 @@ const about = [
 const Content = () => {
     return (
         <Section className={styles.rows + " text-normal"}>
-            <Subtitle className="italic">About / 关于</Subtitle>
-            <Row className={styles.row}>
-                {about[3]}
-            </Row>
-            <Row className={styles.row}>
-                {about[2]}
-            </Row>
-            <Row className={styles.row}>
-                {about[0]}
-            </Row>
-            <Row className={styles.row}>
-                {about[1]}
-            </Row>
-            <Subtitle className="italic">Team / 工作人员</Subtitle>
-            <Row>
-                <p>
-                    <br/>
-                    <span className="red">小玄儿</span><br/>
-                    <span className="italic">Jennifer</span><br/>
-                    创始人 / 策展人 / 运营 / 撰稿<br/>
-                    Founder / Programmer /<br/>
-                    Operating Manager / Writer<br/>
-                    E: <a href="mailto:jennifer@sinoluxfilm.com">jennifer@sinoluxfilm.com</a><br/>
-                    IG: <a target="_blank" rel="noreferrer" href="https://www.instagram.com/xuanyi.zhan/">@xuanyi.zhan</a>
-                </p>
-            </Row>
-            <Row>
-                <p>
-                    <span className="yellow">黄昕</span><br/>
-                    <span className="italic">Xin Huang</span><br/>
-                    创始人 / 策展人<br/>
-                    Founder / Programmer
-                </p>
-            </Row>
-            <Row>
-                <p>
-                    <span className="blue">时雨</span><br/>
-                    <span className="italic">Winnie</span><br/>
-                    运营 / 撰稿 / 记者<br/>
-                    Operating Manager / Writer / Reporter
-                </p>
-            </Row>
-            <Row>
-                <p>
-                    <span className="green">雅淳</span><br/>
-                    <span className="italic">Chuck</span><br/>
-                    运营 / 策划<br/>
-                    Event Coordinator
-                </p>
-            </Row>
-            <Row>
-                <p>
-                    <span className="red">Sandy Long</span><br/>
-                    运营 / 记者<br/>
-                    Operating Manager / Reporter
-                </p>
-            </Row>
-            <Row>
-                <p>
-                    <span className="yellow">Erin Zhou</span><br/>
-                    社团联系<br/>
-                    Marketing & Communication
-                </p>
-            </Row>
-            <Row>
-                <p>
-                    <span className="blue">易桥</span><br/>
-                    <span className="italic">Yi Qiao</span><br/>
-                    财务顾问<br/>
-                    Financial Consultant
-                </p>
-            </Row>
-            <Row>
-                <p>
-                    <span className="green">赵四</span><br/>
-                    <span className="italic">Nicholas Zhao</span><br/>
-                    艺术总监 / 网站<br/>
-                    Art Director / Web Design<br/>
-                    W: IPR<br/>
-                    IG: <a target="_blank" rel="noreferrer" href="https://www.instagram.com/inpraiseofdionysus/">@in praise of dionysus</a><br/>
-                    <br/>
-                </p>
-            </Row>
+            <div className={styles.contentGrid}>
+                <div>
+                    <Subtitle>About / 关于</Subtitle>
+                    <div className={styles.aboutGrid}>
+                        <Row className={styles.row}>
+                            {about[3]}
+                        </Row>
+                        <Row className={styles.row}>
+                            {about[2]}
+                        </Row>
+                        <Row className={styles.row}>
+                            {about[0]}
+                        </Row>
+                        <Row className={styles.row}>
+                            {about[1]}
+                        </Row>
+                    </div>
+                </div>
+                <div>
+                    <Subtitle>Team / 工作人员</Subtitle>
+                    <div className={styles.staffGrid}>
+                        <Row>
+                            <p>
+                                <span className="red">小玄儿</span><br/>
+                                <span className="italic">Jennifer</span><br/>
+                                创始人 / 策展人 / 运营 / 撰稿<br/>
+                                Founder / Programmer /<br/>
+                                Operating Manager / Reporter<br/>
+                                E: <a href="mailto:jennifer@sinoluxfilm.com">jennifer@sinoluxfilm.com</a><br/>
+                                IG: <a target="_blank" rel="noreferrer" href="https://www.instagram.com/xuanyi.zhan/">@xuanyi.zhan</a>
+                            </p>
+                        </Row>
+                        <Row>
+                            <p>
+                                <span className="yellow">黄昕</span><br/>
+                                <span className="italic">Xin Huang</span><br/>
+                                创始人 / 策展人<br/>
+                                Founder / Programmer
+                            </p>
+                        </Row>
+                        <Row>
+                            <p>
+                                <span className="blue">时雨</span><br/>
+                                <span className="italic">Winnie</span><br/>
+                                运营 / 撰稿 / 记者<br/>
+                                Operating Manager / Writer / Reporter
+                            </p>
+                        </Row>
+                        <Row>
+                            <p>
+                                <span className="green">雅淳</span><br/>
+                                <span className="italic">Chuck</span><br/>
+                                运营 / 策划<br/>
+                                Event Coordinator
+                            </p>
+                        </Row>
+                        <Row>
+                            <p>
+                                <span className="red">Sandy Long</span><br/>
+                                运营 / 记者<br/>
+                                Operating Manager / Reporter
+                            </p>
+                        </Row>
+                        <Row>
+                            <p>
+                                <span className="yellow">Erin Zhou</span><br/>
+                                社团联系<br/>
+                                Marketing & Communication
+                            </p>
+                        </Row>
+                        <Row>
+                            <p>
+                                <span className="blue">易桥</span><br/>
+                                <span className="italic">Yi Qiao</span><br/>
+                                财务顾问<br/>
+                                Financial Consultant
+                            </p>
+                        </Row>
+                        <Row>
+                            <p>
+                                <span className="green">赵四</span><br/>
+                                <span className="italic">Nicholas Zhao</span><br/>
+                                技术支持<br/>
+                                Web Design<br/>
+                                W: IPR<br/>
+                                IG: <a target="_blank" rel="noreferrer" href="https://www.instagram.com/inpraiseofdionysus/">@in praise of dionysus</a><br/>
+                                <br/>
+                            </p>
+                        </Row>
+                    </div>
+                </div>
+            </div>
             <p style={{height: "3vh"}}></p>
         </Section>
     )
